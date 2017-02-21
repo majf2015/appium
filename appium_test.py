c510092419f51b2a471f11636325df52ebfd2697
马金凤 <1021008546@qq.com>
2017-02-21 19:15:23 +0800

    学习笔记

--- appium_test.py
+++ appium_test.py
@@ -1,15 +1,19 @@
-from appium import webdriver
-from time import sleep
+from selenium import webdriver
+import os,time
+
+
+PATH=lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
 
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '4.3'
-desired_caps['platformName'] = 'Android Emulator'
-desired_caps['platformName'] = 'com.android.calculator2'
-desired_caps['platformName'] = '.Calculator'
+desired_caps['platformVersion'] = '4.2'
+desired_caps['deviceName'] = 'lenovo S668t'
+desired_caps['app'] = PATH('E:\\appium\\9358mgr_v3.7.0.42_dev_xiaomodo_201702081651.apk')
+desired_caps['appPackage'] = 'com.xmd.manager'
+desired_caps['appActivity'] = 'com.xmd.manager.window.LoginActivity'
 
-driver = webdriver.Remate('http://localhost:4723/wd/hub', desired_caps)
-driver.find_element_by_name("1").click()
-driver.find_element_by_name("5").click()
-driver.find_element_by_name("9").click()
-driver.find_element_by_name("delete").click()
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+time.sleep(5)
+driver.find_element_by_id('com.xmd.manager:id/login').click()
+time.sleep(5)
+driver.quit()
