066e8163f5c0d53c9fc342ab4baa6def08da372f
马金凤 <1021008546@qq.com>
2017-03-14 20:13:40 +0800

    修改登录测试用例

--- appium_test.py
+++ appium_test.py
@@ -10,7 +10,7 @@ class AppTest(unittest.TestCase):
         current_path = sys.path[0]
         conf = ConfigParser.ConfigParser()
         conf.read(os.path.join(current_path,'config.conf'))
-        desired_cap = dict(conf.items('desired_caps'))
+        desired_cap = dict(conf.items('desired_cap'))
         PATH=lambda p:os.path.abspath(os.path.join(os.path.dirname(__file__),p))
 
         desired_caps = {}
@@ -23,6 +23,7 @@ class AppTest(unittest.TestCase):
         desired_caps['noReset'] = desired_cap['noreset']
 
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+        self.account = dict(conf.items('ManagerAccount'))
 
     def tearDown(self):
         self.driver.quit()
@@ -30,9 +31,11 @@ class AppTest(unittest.TestCase):
 
     def test_login(self):
         WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located
-                                            ((By.XPATH, "//android.widget.EditText[contains(@resource-id,'password')]")))
-        self.driver.find_element_by_xpath("//android.widget.EditText[contains(@resource-id,'password')]").send_keys('999999')
-        WebDriverWait(self.driver, 5).until(expected_conditions.visibility_of_element_located
-                               ((By.XPATH, "//android.widget.Button[contains(@resource-id,'login')]")))
+                                            ((By.XPATH, "//android.widget.EditText[contains(@resource-id,'username')]")))
+        self.driver.find_element_by_xpath("//android.widget.EditText[contains(@resource-id,'username')]").clear()
+        self.driver.find_element_by_xpath("//android.widget.EditText[contains(@resource-id,'username')]")\
+            .send_keys(self.account['user'])
+        self.driver.find_element_by_xpath("//android.widget.EditText[contains(@resource-id,'password')]")\
+            .send_keys(self.account['password'])
         self.driver.find_element_by_xpath("//android.widget.Button[contains(@resource-id,'login')]").click()
-        time.sleep(10)
+
